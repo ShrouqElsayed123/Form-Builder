c780df43a02ce82dee844697cfa0ef944a59f0c2
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-03-25 12:12:35 -0700

    create home page (charts) in Dashboard

--- src/Pages/DashboardPages/FormPage.jsx
+++ src/Pages/DashboardPages/FormPage.jsx
@@ -7,11 +7,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 import AppBar1 from '../../Components/DashboardComponent/AppBar/AppBar1';
 import SideBar1 from '../../Components/DashboardComponent/SideBar/SideBar1';
-import { BarChart } from '@mui/x-charts/BarChart'
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
-import { dataset, valueFormatter } from './weather';
-import Paper from '@mui/material/Paper';
 
+import Paper from '@mui/material/Paper';
+import styles from './FormPage.module.css'
+import { Group, ListAlt, TrendingUp } from '@mui/icons-material';
+import { PieChart } from '@mui/x-charts';
 
 
 const drawerWidth = 240;
@@ -54,27 +54,10 @@ export const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 ///////////////////////////
 
-const chartSetting = {
-    yAxis: [
-      {
-        label: 'rainfall (mm)',
-        
-      },
-    ],
-    
-    
-    series: [{ dataKey: 'seoul', label: 'Seoul rainfall', valueFormatter }],
-    width: 500,
-    height: 400,
-    sx: {
-      [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-        transform: 'translateX(-10px)',
-      },
-    },
-  };
+
   const DemoPaper = styled(Paper)(({ theme }) => ({
-    width: 120,
-    height: 120,
+    width:"fit-content",
+    height: "fit-content",
     padding: theme.spacing(2),
     ...theme.typography.body2,
     textAlign: 'center',
@@ -111,16 +94,78 @@ export default function FormPage() {
             
         </DrawerHeader>
       {/* <FormTable /> */}
-      <div className='d-flex align-items-center justify-content-center'>
-      <DemoPaper square={false}>rounded corners</DemoPaper>
-      <BarChart
-        dataset={dataset}
-        xAxis={[
-          { scaleType: 'band', dataKey: 'month'},
-        ]}
-        {...chartSetting}
+      <div className='d-flex align-items-center justify-content-center flex-column gap-5 mt-5'>
+     
+      <div className='d-flex align-items-center justify-content-center gap-5'>
+      <DemoPaper className={`${styles.dashboard1paper} d-flex align-items-center justify-content-center gap-3  `} elevation={1}  >
+      <ListAlt  sx={{
+        color:"#4EB100",
+        backgroundColor:"#4db1003d",
+        padding:"8px",
+        borderRadius:"50%",
+        fontSize:"60px"
+      }} />
+       <div>
+       <p>Numbers Of Forms</p>
+       <h3>200 <TrendingUp/></h3>
+       </div>
+      </DemoPaper>
+
+      <DemoPaper className={`${styles.dashboard1paper} d-flex align-items-center justify-content-center gap-3  `} elevation={1}  >
+      <Group 
+      sx={{
+        color:"#4EB100",
+        backgroundColor:"#4db1003d",
+        padding:"8px",
+        borderRadius:"50%",
+        fontSize:"60px"
+      }} 
       />
+      <div>
+      <p>Numbers Of Users</p>
+      <h3>200 <TrendingUp/></h3>
       </div>
+      </DemoPaper>
+      </div>
+
+      <div>
+      <DemoPaper elevation={3} >
+      <PieChart
+      
+      series={[
+        {
+          data: [
+            { id: 0, value: 10, label: 'saved forms' ,color:"#95D066"},
+            { id: 1, value: 15, label: 'published Forms',color:"#E1C269" },
+            
+           
+          ],
+        },
+      ]}
+      slotProps={{
+        legend: {
+       
+          position: {
+            vertical: 'middle',
+            horizontal: 'right',
+          },
+          itemMarkWidth: 20,
+          itemMarkHeight: 2,
+          markGap: 5,
+          itemGap: 10,
+        }
+      }}
+      width={400}
+      height={200}
+      margin={{ right: 200 }}
+    />
+      </DemoPaper>
+      </div>
+
+
+      </div>
+
+     
       </Main>
     </Box>
   );
